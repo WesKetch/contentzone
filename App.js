e3889eeb8e96ae01fde472b3564fb9f80152bda1
wes ketchum <wesleyketchum@gmail.com>
2020-01-29 14:19:13 -0500

    wip navigation

--- App.js
+++ App.js
@@ -8,16 +8,28 @@ import {
   SafeAreaView,
   FlatList,
   Image,
-  ImageBackground
+  ImageBackground,
+  Button
 } from "react-native";
 
 import Header from "./components/Header";
 import ContentCard from "./components/ContentCard";
 
+import { createAppContainer } from "react-navigation";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { createStackNavigator } from "react-navigation-stack";
+import { BottomNavigation, Text as PaperText } from "react-native-paper";
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+    marginHorizontal: 16
   }
 });
 
@@ -33,7 +45,7 @@ async function getContent() {
   }
 }
 
-export default function App() {
+const App = props => {
   const [articles, setState] = useState([]);
 
   useEffect(() => {
@@ -49,10 +61,32 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Header />
       <ScrollView>
+        <Text style={styles.text}>Top News Stories</Text>
         {articles.map((article, index) => (
           <ContentCard key={index} article={article} />
         ))}
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
+
+const Sports = () => (
+  <View>
+    <Text>hi</Text>
+  </View>
+);
+
+const Home = createMaterialBottomTabNavigator(
+  {
+    News: App,
+    LifeStyle: Sports
+  },
+  {
+    initialRouteName: "News",
+    activeColor: "orange",
+    inactiveColor: "black",
+    barStyle: { backgroundColor: "#F0F0F0" }
+  }
+);
+
+export default createAppContainer(Home);
